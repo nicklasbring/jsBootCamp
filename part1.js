1ef9c7737207ee1f1cc63d05fd381af57eb54fc7
Nicklas Bring Nielsen <nicklas.bring.nielsen@gmail.com>
2019-09-25 23:08:10 +0200

    Exercise 1 done

---
+++ part1.js
@@ -0,0 +1,16 @@
+
+//exercise 1  
+console.log("/////Exercise 1/////");
+function sum(n) {
+  result = (n * (n + 1)) / 2;
+    console.log(result);
+}
+  sum(3); // => 6
+  sum(4); // => 10
+  sum(5); // => 15
+  sum(6); // => 21
+  sum(7); // => 28
+  sum(8); // => 36
+
+
+  
